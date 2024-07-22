74f9a51680265abbb8fb1e6c9920ef99d27b1d8b
Yusra Obaidullah <wasiyusra@gmail.com>
2024-07-22 22:07:31 +0100

    API and Web UI Testing

--- tests/step_defs/test_cucumber_steps.py
+++ tests/step_defs/test_cucumber_steps.py
@@ -1,20 +1,42 @@
-from pytest_bdd import scenario, given, when, then
+from pytest_bdd import scenarios, parsers, given, when, then
 
 from cucumbers import CucumberBasket
 
-@scenario('../features/cucumbers.feature', 'Add cucumbers to a basket')
 
-def test_add():
-    pass
+scenarios('../features/cucumbers.feature')
 
-@given("the basket has 2 cucumbers", target_fixture='basket')
-def basket():
-    return CucumberBasket(initial_count=2)
 
-@when("4 cucumbers are added to the basket")
-def add_cucumbers(basket):
-    basket.add(4)
+CONVERTERS = {
+  'initial': int,
+  'some': int,
+  'total': int,
+}
 
-@then("the basket contains 6 cucumbers")
-def basket_has_total(basket):
-    assert basket.count == 6
+
+@given(
+  parsers.parse('the basket has "{initial}" cucumbers'),
+  target_fixture='basket',
+  converters=CONVERTERS)
+def basket(initial):
+  return CucumberBasket(initial_count=initial)
+
+
+@when(
+  parsers.parse('"{some}" cucumbers are added to the basket'),
+  converters=CONVERTERS)
+def add_cucumbers(basket, some):
+  basket.add(some)
+
+
+@when(
+  parsers.parse('"{some}" cucumbers are removed from the basket'),
+  converters=CONVERTERS)
+def remove_cucumbers(basket, some):
+  basket.remove(some)
+
+
+@then(
+  parsers.parse('the basket contains "{total}" cucumbers'),
+  converters=CONVERTERS)
+def basket_has_total(basket, total):
+  assert basket.count == total
